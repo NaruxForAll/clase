9d3cc632f2f7281bc76b3f04493a8e08114fb032
NaruxForAll <naruxtv@gmail.com>
2021-01-26 09:29:36 +0100

    Leeme el CSS Y mi Archivo

---
+++ resources/views/home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario Base</title>
+    <link rel="stylesheet" type="text/css" href="/css/home.css" media="screen" />
+</head>
+<body>
+    <header>
+        <div><h1>Formulario Base</h1></div>
+    </header>
+    <main>
+        <form action="" method="get">
+            <input name="" type="text" placeholder="Texto de prueba"> 
+            <input name="" type="text" placeholder="Texto de prueba"> 
+            <input name="" type="text" placeholder="Texto de prueba"> 
+            <input name="" type="text" placeholder="Texto de prueba">
+        </form>
+        <button id="">Enviar</button>
+    </main>
+</body>
+</html>
