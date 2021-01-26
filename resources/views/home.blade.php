7dd9e9cd648995b35e7aafb425e1a08b9efe4968
NaruxForAll <naruxtv@gmail.com>
2021-01-26 11:55:17 +0100

    V7 JS

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulario Base</title>
     <link rel="stylesheet" type="text/css" href="/css/home.css" media="screen" />
-    <script src="/js/app.js"></script>
 </head>
 <body>
     <div id="primary">
@@ -39,5 +38,6 @@
         </table>
     </footer>
     </div>
+    <script src="/js/app.js"></script>
 </body>
 </html>
