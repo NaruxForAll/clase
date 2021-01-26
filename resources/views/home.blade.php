a3f1a8a4dc9d5d8d22cd3d813ba16d9f0a3ba080
NaruxForAll <naruxtv@gmail.com>
2021-01-26 11:11:11 +0100

    V3

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,6 +22,22 @@
         <button id="">Enviar</button>
         <div>
     </main>
+    <footer>
+        <table>
+            <thead>
+                <th>Columna 1</th>
+                <th>Columna 2</th>
+                <th>Columna 3</th>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Dato C1</td>
+                    <td>Dato C2</td>
+                    <td>Dato C3</td>
+                </tr>
+            </tbody>
+        </table>
+    </footer>
     </div>
 </body>
 </html>
