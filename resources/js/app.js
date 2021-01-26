9586e1e21140f56713134f98d5f63d821a3e3e5c
NaruxForAll <naruxtv@gmail.com>
2021-01-26 11:58:22 +0100

    V9 JS

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,8 +4,7 @@ document.getElementById("send").addEventListener("click", ()=>{
     var title1 = document.getElementById("title1").value
     var title2 = document.getElementById("title2").value
     var title3 = document.getElementById("title3").value
-    console.log(title1)
-    if(title1 != null && title2 != null && title3 != null){
+    if(title1 != "" && title2 != "" && title3 != ""){
         document.getElementById("data").innerHTML += "<tr><td>"+title1+"</td>"+"<td>"+title2+"</td>"+"<td>"+title3+"</td></tr>"
     } else{
         alert("Te has dejado algún campo vacío")
