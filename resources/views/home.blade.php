9b854ff26a3ed61ed6f2684906b8701a8e609fd8
NaruxForAll <naruxtv@gmail.com>
2021-01-26 11:35:00 +0100

    plop

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulario Base</title>
     <link rel="stylesheet" type="text/css" href="/css/home.css" media="screen" />
+    <script src="/js/app.js"></script>
 </head>
 <body>
     <div id="primary">
