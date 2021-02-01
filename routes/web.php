d746ad5958d2d6caeb263669ad8a42283df9d298
NaruxForAll <naruxtv@gmail.com>
2021-02-01 10:39:49 +0100

    V1234

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('/posts', 'App\Http\Controllers\PostController', [
+Route::resource('posts', 'App\Http\Controllers\PostController', [
     'names' => [
         'index' => 'posts',
         'create' => 'posts_create',
