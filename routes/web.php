fa1ceae1576bf0631276bbaec26d724ab920e3ce
NaruxForAll <naruxtv@gmail.com>
2021-02-01 09:24:11 +0100

    fix

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/', function () {
-    return view('form');
+    return view('home');
 });
 
 Route::resource('posts', 'App\Http\Controllers\PostController' , [
